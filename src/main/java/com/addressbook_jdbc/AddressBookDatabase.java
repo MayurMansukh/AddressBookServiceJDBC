2a59da4adad376858b90f401cc84ddf66785dd16
MayurMansukh <manojmansukh7004>
2021-03-26 11:39:24 +0530

    add logic read data from database

--- src/main/java/com/addressbook_jdbc/AddressBookDatabase.java
+++ src/main/java/com/addressbook_jdbc/AddressBookDatabase.java
@@ -28,5 +28,46 @@ public class AddressBookDatabase {
         return connection;
     }
 
+    public List<AddressBookData> readData(){
+        String Sql_Query = "Select * from AddressBookTable";
+        List<AddressBookData> addressBookData = new ArrayList<>();
+        try {
+            Connection connection = this.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(Sql_Query);
+            while (resultSet.next()) {
+                String FirstName=resultSet.getString(1);
+                String LastName=resultSet.getString(2);
+                String AddressBookType=resultSet.getString(3);
+                String Address=resultSet.getString(4);
+                String city=resultSet.getString(5);
+                String State=resultSet.getString(6);
+                int Zip=resultSet.getInt(7);
+                String Email=resultSet.getString(8);
+                String AddressBookName=resultSet.getString(9);
+
+                System.out.println();
+                System.out.println("FirstName="+FirstName);
+                System.out.println("LastName=" + LastName);
+                System.out.println("AddressBookType=" + AddressBookType);
+                System.out.println("City=" + city);
+                System.out.println("State=" + State);
+                System.out.println("Zip=" + Zip);
+                System.out.println("Email=" + Email);
+                System.out.println("AddressBookName"+AddressBookName);
+
+                AddressBookData addressBookData1=new AddressBookData(resultSet.getString(1),resultSet.getString(2),
+                        resultSet.getString(3),resultSet.getString(4),resultSet.getString(5),resultSet.getString(6),
+                        resultSet.getInt(7),resultSet.getString(8),resultSet.getString(9));
+
+                addressBookData.add(addressBookData1);
+            }
+            statement.close();
+            connection.close();
+        } catch (SQLException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return addressBookData;
+    }
 
 }
--- src/test/java/com/addressbook_jdbc/AddressBookDatabaseTest.java
+++ src/test/java/com/addressbook_jdbc/AddressBookDatabaseTest.java
@@ -6,5 +6,13 @@ import java.util.List;
 
 class AddressBookDatabaseTest {
 
+    @Test
+    void readData_And_returnCount_Test() {
+        AddressBookDatabase addressBookDatabase=new AddressBookDatabase();
+        List<AddressBookData>addressBookData1=addressBookDatabase.readData();
+        System.out.println(addressBookData1.size());
+        Assertions.assertEquals(4,addressBookData1.size());
+    }
+
 
 }
